15ce91c753d8007a3e48cd30955ccbf712872253
MD Naimur Rahman <naimurtsc567@gmail.com>
2023-09-30 10:45:24 +0600

    Wishlist module updated

--- backend/src/app/modules/wishlist/wishlist.service.ts
+++ backend/src/app/modules/wishlist/wishlist.service.ts
@@ -13,21 +13,39 @@ const addWishlist = async (payload: IWishlist): Promise<IWishlist> => {
     throw new ApiError(httpStatus.FORBIDDEN, "Failed to Wishlist Product!");
   }
 
-  const isProductExists = await Products.findById({ _id: productID });
+  const isProductExists = await Products.findById(
+    { _id: productID },
+    {
+      _id: 1,
+    }
+  ).lean();
   if (!isProductExists) {
     throw new ApiError(httpStatus.NOT_FOUND, "Product Does Not Exist's!");
   }
 
-  const isUserExists = await Users.findById({ _id: userID });
+  const isUserExists = await Users.findById(
+    { _id: userID },
+    {
+      _id: 1,
+    }
+  ).lean();
   if (!isUserExists) {
     throw new ApiError(httpStatus.NOT_FOUND, "User Does Not Exist's!");
   }
 
-  const isWishlistExists = Wishlist.findOne({ productID: productID });
+  const isWishlistExists = await Wishlist.findOne(
+    {
+      productID: productID,
+      userID: userID, // Include the userID condition
+    },
+    {
+      _id: 1,
+    }
+  ).lean();
 
-  if (await isWishlistExists) {
+  if (isWishlistExists) {
     throw new ApiError(
-      httpStatus.FORBIDDEN,
+      httpStatus.CONFLICT,
       "Product Already Added to Wishlist!"
     );
   }
@@ -38,9 +56,10 @@ const addWishlist = async (payload: IWishlist): Promise<IWishlist> => {
 
 // Get wishlists By Product ID
 const getWishlistsByUserID = async (userID: string): Promise<IWishlist[]> => {
-  const wishlists = await Wishlist.find({ userID: userID }).populate(
-    "productID"
-  );
+  const wishlists = await Wishlist.find({ userID: userID }).populate({
+    path: "productID",
+    select: "_id images.i1 name status price",
+  });
   if (wishlists.length === 0) {
     throw new ApiError(httpStatus.NOT_FOUND, "0 Product's Found");
   }
@@ -51,7 +70,13 @@ const getWishlistsByUserID = async (userID: string): Promise<IWishlist[]> => {
 // Delete Wishlist Product
 const deleteWishlist = async (payload: IDeleteWishlist): Promise<null> => {
   const { userID, wishlistId } = payload;
-  const isExistsWishlist = await Wishlist.findById({ _id: wishlistId });
+  const isExistsWishlist = await Wishlist.findById(
+    { _id: wishlistId },
+    {
+      _id: 0,
+      userID: 1,
+    }
+  );
   if (!isExistsWishlist) {
     throw new ApiError(
       httpStatus.NOT_FOUND,
