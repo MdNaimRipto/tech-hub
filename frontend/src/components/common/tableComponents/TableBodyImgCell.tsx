32615930a25d01d9f46b5255cc40228eb45d647a
MD Naimur Rahman <naimurtsc567@gmail.com>
2023-11-08 18:24:16 +0600

    Get All Products for admin page design updated

--- frontend/src/components/common/tableComponents/TableBodyImgCell.tsx
+++ frontend/src/components/common/tableComponents/TableBodyImgCell.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const TableBodyImgCell = ({ src, sx }: { src: string; sx?: string }) => {
   return (
     <TableCell component="th" scope="row" align="center">
-      <Image src={src} alt="Product Image" width={80} height={80} />
+      <Image src={src} alt="Product Image" width={80} height={80} priority />
     </TableCell>
   );
 };
